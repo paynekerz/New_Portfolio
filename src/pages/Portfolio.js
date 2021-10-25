cfa3605088979c679af69d057bf6f9d5fa9312a8
Payne Kerz <paynekerz@gmail.com>
2021-10-25 10:54:44 -0700

    portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,33 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import "../styles/Portfolio.css"
 
 export default function Portfolio() {
 
     return (
-        <h1> This is the Portfolio </h1>
+        <div className="background">
+            <div className="cardHolder">
+                <div className="projectCard">
+                    <h2>inTents</h2>
+                    <p>A review/blog app for national parks</p>
+                    <p>Role: React functionality and CSS design.</p>
+                    <p>Technologies: React.js, MongoDB, National Parks API</p>
+                    <div className="linkHolder">
+                        <h3><a className="linkGithub" href="https://github.com/paynekerz/i-n-t-e-n-t-s" target="_target" rel="noreferrer noopener">Github</a></h3>
+                        <h3><a className="linkHeroku" href="https://i-n-t-e-n-t-s.herokuapp.com/" target="_target" rel="noreferrer noopener">Heroku</a></h3>
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <h2>Cities2See</h2>
+                    <p>An app to keep track of what cities you want to/have visited</p>
+                    <p>Role: React functionality and CSS design.</p>
+                    <p>Technologies: React.js, MongoDB, National Parks API</p>
+                    <div className="linkHolder">
+                        <h3><a className="linkGithub" href="https://github.com/paynekerz/Cities2See" target="_target" rel="noreferrer noopener">Github</a></h3>
+                        <h3><a className="linkHeroku" href="https://cities2see.herokuapp.com/" target="_target" rel="noreferrer noopener">Heroku</a></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
