5823f2b22cccf247619139044d31e242a7b2d172
Payne Kerz <paynekerz@gmail.com>
2021-10-25 10:53:51 -0700

    app.js changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "../src/components/Navbar";
 import AboutMe from "../src/pages/AboutMe";
 import Portfolio from "../src/pages/Portfolio";
-import ContactMe from "../src/pages/ContactMe";
 import Resume from "../src/pages/Resume";
 import Footer from "../src/components/Footer";
 import "./styles/App.css";
@@ -10,22 +9,23 @@ import "./styles/App.css";
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Route exact path="/">
-        <AboutMe />
-      </Route>
-      <Route exact path="/Portfolio">
-        <Portfolio />
-      </Route>
-      <Route exact path="/ContactMe">
-        <ContactMe />
-      </Route>
-      <Route exact path="/Resume">
-        <Resume />
-      </Route>
-      <Footer />
-    </Router>
+      <Router>
+        <Navbar />
+        <Route exact path="/">
+          <AboutMe />
+        </Route>
+        <Route exact path="/Portfolio">
+          <Portfolio />
+        </Route>
+        <Route
+          path="/Resume"
+          component={() => {
+            window.location.href = "https://docs.google.com/document/d/1BZ0vLxMGqkUt8_-1zJAdqAecaTq3Kqcy-85jnPSk6uI/edit?usp=sharing";
+            return null;
+          }}
+        />
+        <Footer />
+      </Router>
     </>
   );
 }
