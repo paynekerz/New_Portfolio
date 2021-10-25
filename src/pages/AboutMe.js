f229d7a47aa9422b6122c2293691b37472aba783
Payne Kerz <paynekerz@gmail.com>
2021-10-25 10:55:11 -0700

    about changes

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -15,19 +15,23 @@ export default function AboutMe() {
     const classes = useStyles();
 
     return (
-        <Container>
-        <div className="flexDiv">
-            <div className="aboutPic">
-                <img src="/images/profile-pic.jpeg" className={classes.profile} alt=""/>
+        <div className="background">
+            <Container>
+            <div className="flexDiv">
+                <div className="aboutPic">
+                    <img src="/images/profile-pic.jpeg" className={classes.profile} alt=""/>
+                </div>
+                <div className="aboutCont">
+                    <div className="aboutDesc">
+                        <p>Welcome to my Portfolio! My name's Payne Kerz .</p>
+                        <p>I am an ambitious Full Stack Web Developer who can build responsive websites and single-page applications from the ground up – from concept, navigation, layout, and programming. </p>
+                        <p>In my spare time I like to read science fiction books, watch documentaries, and listen to podcasts like the Joe Rogan Experience.</p>
+                        <p>I also enjoy working out and hiking mountain trails!</p>
+                        <p>I will keep this page updated with new projects. Come again soon!</p>
+                    </div>
+                </div>
             </div>
-            <div className="aboutDesc">
-                <p>Welcome to my Portfolio! My name is Payne Kerz .</p>
-                <p>I am an ambitious Full Stack Web Developer who can build responsive websites and single-page applications from the ground up – from concept, navigation, layout, and programming. </p>
-                <p>I am a fast learner, hard worker, and ultimate team player proficient in a wide array of technical languages.</p>
-                <p>I have a proven aptitude in problem-solving while maintaining a positive professional demeanor while providing an exceptional customer experience and can easily work efficiently independently or adapt quickly to a team environment.</p>
-                <p>I will keep this page updated with new projects. Come again soon!</p>
-            </div>
+            </Container>
         </div>
-        </Container>
     )
 }
